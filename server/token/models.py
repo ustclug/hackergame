956b85ae92b1e4d807a8fcdc3d3db7b9e7e9b745
Hypercube <hypercube@0x01.me>
2019-09-07 19:42:07 +0800

    improve token

--- server/token/models.py
+++ server/token/models.py
@@ -1,7 +1,13 @@
+import base64
+import OpenSSL
+
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import models
 
 User = get_user_model()
+private_key = OpenSSL.crypto.load_privatekey(OpenSSL.crypto.FILETYPE_PEM,
+                                             settings.PRIVATE_KEY)
 
 
 class Token(models.Model):
@@ -10,3 +16,13 @@ class Token(models.Model):
 
     def __str__(self):
         return self.token
+
+    @classmethod
+    def get(cls, user):
+        try:
+            return user.token
+        except cls.DoesNotExist:
+            id = str(user.pk)
+            sig = OpenSSL.crypto.sign(private_key, id.encode(), 'sha256')
+            token = id + ':' + base64.b64encode(sig).decode()
+            return Token.objects.create(user=user, token=token)
