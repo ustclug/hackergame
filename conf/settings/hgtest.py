ecd8f055408941021779f3031ff4d9d0661a03bb
Hypercube <hypercube@0x01.me>
2019-10-10 01:42:33 +0800

    add google and microsoft app_id for hgtest

--- conf/settings/hgtest.py
+++ conf/settings/hgtest.py
@@ -27,4 +27,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.s.ustclug.org'
 EMAIL_SUBJECT_PREFIX = '[HG-TEST] '
 
+GOOGLE_APP_ID = '2574063612-p2ss2hgg9rr7c67n9d0e4g3l6j9gk8v2' \
+                '.apps.googleusercontent.com'
+MICROSOFT_APP_ID = '6a243fe9-a603-4c6e-b6bd-5af20b7f460e'
 SMS_ACCESS_KEY_ID = 'LTAI4FmgeKHNWB7WbTwTP7d9'
