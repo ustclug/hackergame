74821760dfc7cbf11dffa7164954eca537a2ddb8
Hypercube <hypercube@0x01.me>
2019-10-10 22:13:40 +0800

    add google and microsoft app ids for hackergame

--- conf/settings/hackergame.py
+++ conf/settings/hackergame.py
@@ -26,4 +26,7 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.s.ustclug.org'
 
+GOOGLE_APP_ID = '2574063612-kstsrirbttbimgk2da2ju1mmbh8t0ogk' \
+                '.apps.googleusercontent.com'
+MICROSOFT_APP_ID = '6a243fe9-a603-4c6e-b6bd-5af20b7f460e'
 SMS_ACCESS_KEY_ID = 'LTAI4FmgeKHNWB7WbTwTP7d9'
