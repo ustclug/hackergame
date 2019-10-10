2ec865aa7c1c2c3bb5989e6656c972e66c68e9ce
zzh1996 <zhengzihan1996@gmail.com>
2019-10-11 01:08:45 +0800

    show prompt instead of always loading before game starts

--- frontend/static/trend.js
+++ frontend/static/trend.js
@@ -10,7 +10,13 @@ function drawchart() {
     ))),
     axios.post('/admin/trigger/', {method: 'get_all'}),
   ]).then(([user_reqs, {data: {value: triggers}}]) => {
-    let starttime = new Date(triggers.find(i => i.state).time);
+    let starttime = triggers.find(i => i.state);
+    if (!starttime || new Date(starttime.time) > new Date()) {
+      document.getElementById('chart').innerHTML = '比赛尚未开始';
+      return;
+    } else {
+      starttime = new Date(starttime.time);
+    }
     let endtime = [...triggers].reverse().find(i => !i.state);
     endtime = endtime ? new Date(endtime.time) : new Date();
     if (endtime > new Date()) {
