b1eafbb1a0787fb8ad430b2b7451de316d7e0033
Hypercube <hypercube@0x01.me>
2019-10-12 13:26:18 +0800

    update neu login provider

--- frontend/auth_providers/neu.py
+++ frontend/auth_providers/neu.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 
-from django.core.mail import EmailMessage
+from django.conf import settings
+from django.core.mail import EmailMessage, get_connection
 from django.shortcuts import redirect
 from django.urls import path
 
@@ -26,11 +27,21 @@ class GetCodeView(BaseGetCodeView):
 
     def send(self, identity, code):
         url = self.request.build_absolute_uri("/")
-        EmailMessage(
-            subject=f'登录校验码：{code}',
-            body=f'{code}\n请使用该校验码登录 {url}\n',
-            to=[identity],
-        ).send()
+        with get_connection(
+            backend='django.core.mail.backends.smtp.EmailBackend',
+            host='smtp.sendgrid.net',
+            port=587,
+            username='apikey',
+            password=settings.SENDGRID_API_KEY,
+            use_tls=True,
+            use_ssl=False,
+        ) as connection:
+            EmailMessage(
+                subject=f'登录校验码：{code}',
+                body=f'{code}\n请使用该校验码登录 {url}\n',
+                to=[identity],
+                connection=connection,
+            ).send()
 
 
 urlpatterns = [
