684132870841e8c3b6015c93c764f55b8e3bd729
Hypercube <hypercube@0x01.me>
2019-10-12 23:59:19 +0800

    clean

--- frontend/management/commands/import_data.py
+++ frontend/management/commands/import_data.py
@@ -28,75 +28,82 @@ class Command(BaseCommand):
         Account.objects.create(provider='debug', identity='root', user=root)
 
         for dir in pathlib.Path(source_dir).iterdir():
-            if dir.is_dir() and not dir.name.startswith('.'):
-                print(f'Processing {dir.name}...', end=' ')
-                for file in dir.iterdir():
-                    if file.name.upper() == 'README.MD':
-                        readme = file
-                        break
+            if not dir.is_dir() or dir.name.startswith('.'):
+                continue
+            print(f'Processing {dir.name}...', end=' ')
+            for file in dir.iterdir():
+                if file.name.upper() == 'README.MD':
+                    readme = file
+                    break
+            else:
+                print('Readme file not found')
+                continue
+
+            with open(readme) as f:
+                lines = f.readlines()
+            if lines[0] != '---\n':
+                print('Header not found in readme')
+                continue
+            pos = lines.index('---\n', 1)
+            headers = lines[1:pos]
+            body = ''.join(lines[pos + 1:])
+            metadata = {}
+            for line in headers:
+                pos = line.find(':')
+                key = line[:pos].strip()
+                value = line[pos + 1:].strip()
+                metadata[key] = value
+
+            if not int(metadata.get('enabled', 0)):
+                print('Not enabled')
+                continue
+            url = metadata['url']
+            if url and not url.startswith('http://'):
+                source = dir / url
+                target = pathlib.Path(files_dir) / url
+                shutil.copy(source, target)
+                url = '/media/' + url
+
+            if 'extrafile' in metadata:
+                for file in metadata['extrafile'].split(', '):
+                    source = dir / file
+                    target = pathlib.Path(files_dir) / file
+                    shutil.copy(source, target)
+
+            flag_flags = metadata['flag'].split(', ')
+            flag_scores = metadata['score'].split(', ')
+            if len(flag_flags) > 1:
+                flag_names = metadata['flagnames'].split(', ')
+            else:
+                flag_names = ['']
+
+            flags = []
+            for i in range(len(flag_flags)):
+                if flag_flags[i].startswith('f"'):
+                    assert flag_flags[i].startswith('f"flag{{')
+                    assert flag_flags[i].endswith('}}"')
+                    flag_type = 'expr'
                 else:
-                    print('Readme file not found')
-                    continue
+                    assert flag_flags[i].startswith('flag{')
+                    assert flag_flags[i].endswith('}')
+                    flag_type = 'text'
+                flags.append({
+                    'name': flag_names[i],
+                    'score': flag_scores[i],
+                    'type': flag_type,
+                    'flag': flag_flags[i],
+                })
 
-                with open(readme) as f:
-                    lines = f.readlines()
-                if lines[0] != '---\n':
-                    print('Header not found in readme')
-                    continue
-                pos = lines.index('---\n', 1)
-                headers = lines[1:pos]
-                body = ''.join(lines[pos + 1:])
-                metadata = {}
-                for line in headers:
-                    pos = line.find(':')
-                    key = line[:pos].strip()
-                    value = line[pos + 1:].strip()
-                    metadata[key] = value
+            Challenge.create(
+                Context(root),
+                name=metadata['title'],
+                category=metadata['category'],
+                detail=markdown.markdown(body, extensions=['fenced_code']),
+                url=url,
+                prompt='flag{...}',
+                index=int(metadata['index']),
+                enabled=True,
+                flags=flags,
+            )
 
-                if 'enabled' in metadata and int(metadata['enabled']):
-                    url = metadata['url']
-                    if url and not url.startswith('http://'):
-                        source = dir / url
-                        target = pathlib.Path(files_dir) / url
-                        shutil.copy(source, target)
-                        url = '/media/' + url
-
-                    if 'extrafile' in metadata:
-                        for file in metadata['extrafile'].split(', '):
-                            source = dir / file
-                            target = pathlib.Path(files_dir) / file
-                            shutil.copy(source, target)
-
-                    flag_flags = metadata['flag'].split(', ')
-                    flag_scores = metadata['score'].split(', ')
-                    if len(flag_flags) > 1:
-                        flag_names = metadata['flagnames'].split(', ')
-                    else:
-                        flag_names = ['']
-
-                    flags = []
-                    for i in range(len(flag_flags)):
-                        assert flag_flags[i].startswith('f"flag{{') or flag_flags[i].startswith('flag{')
-                        assert flag_flags[i].endswith('}}"') or flag_flags[i].endswith('}')
-                        flags.append({
-                            'name': flag_names[i],
-                            'score': flag_scores[i],
-                            'type': 'expr' if flag_flags[i].startswith('f"') else 'text',
-                            'flag': flag_flags[i],
-                        })
-
-                    Challenge.create(
-                        Context(root),
-                        name=metadata['title'],
-                        category=metadata['category'],
-                        detail=markdown.markdown(body, extensions=['fenced_code']),
-                        url=url,
-                        prompt='flag{...}',
-                        index=int(metadata['index']),
-                        enabled=True,
-                        flags=flags,
-                    )
-
-                    print('Succeeded')
-                else:
-                    print('Not enabled')
+            print('Succeeded')
