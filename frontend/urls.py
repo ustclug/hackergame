0dfaba895be94af7fd83def23cb7baf3a357ea7b
Hypercube <hypercube@0x01.me>
2022-10-17 00:00:00 +0000

    frontend: serve media when debug = True

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -43,4 +45,4 @@ urlpatterns = [
     path('admin/trigger/', views.TriggerAdminView.as_view()),
     path('admin/user/', views.UserAdminView.as_view()),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
