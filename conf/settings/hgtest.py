7effdbe961a713a854da4e4185522a512085a38c
taoky <me@taoky.moe>
2023-08-10 01:17:17 +0800

    Replace python-memcached with pymemcache
    python-memcached is deprecated in Django 3.2
    and will be removed in Django 4.1.

--- conf/settings/hgtest.py
+++ conf/settings/hgtest.py
@@ -16,7 +16,7 @@ DATABASES = {
 }
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
         'LOCATION': '127.0.0.1:11211',
         'TIMEOUT': 3600,
         'KEY_PREFIX': 'hgtest',
