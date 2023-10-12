d9c99ca13f4a2a6e73895145b2114abad4f01da0
taoky <me@taoky.moe>
2023-10-12 22:15:17 +0800

    auth: strip cas out of ustc.py

--- frontend/auth_providers/ustc.py
+++ frontend/auth_providers/ustc.py
@@ -7,45 +7,30 @@ from django.contrib import messages
 from django.shortcuts import redirect
 from django.urls import path
 
+from typing import Optional
+
 from ..models import UstcSnos
-from .base import BaseLoginView
+from .cas import CASBaseLoginView
 
 
-class LoginView(BaseLoginView):
+class LoginView(CASBaseLoginView):
     provider = 'ustc'
     group = 'other'  # XXX: 先加入 other，确认在读后移动至 ustc
     service: str
     ticket: str
     sno: str
 
-    def get(self, request):
-        self.service = request.build_absolute_uri('/accounts/ustc/login/')
-        self.ticket = request.GET.get('ticket')
-        if not self.ticket:
-            return redirect('https://passport.ustc.edu.cn/login?' +
-                            urlencode({'service': self.service}))
-        if self.check_ticket():
-            self.login(sno=self.sno)
-        return redirect('hub')
+    cas_name = '统一身份认证平台'
+    cas_login_url = 'https://passport.ustc.edu.cn/login'
+    cas_service_validate_url = 'https://passport.ustc.edu.cn/serviceValidate'
 
-    def check_ticket(self):
-        try:
-            with urlopen(
-                'https://passport.ustc.edu.cn/serviceValidate?' +
-                urlencode({'service': self.service, 'ticket': self.ticket}),
-                timeout=15,
-            ) as req:
-                tree = ElementTree.fromstring(req.read())[0]
-        except URLError:
-            messages.error(self.request, '连接统一身份认证平台出错')
-            return False
-        cas = '{http://www.yale.edu/tp/cas}'
-        if tree.tag != cas + 'authenticationSuccess':
-            messages.error(self.request, '登录失败')
-            return False
-        self.identity = tree.find('attributes').find(cas + 'gid').text.strip()
-        self.sno = tree.find(cas + 'user').text.strip()
-        return True
+    def check_ticket(self) -> Optional[ElementTree.Element]:
+        tree = super().check_ticket()
+        if not tree:
+            return None
+        self.identity = tree.find('attributes').find(self.YALE_CAS_URL + 'gid').text.strip()
+        self.sno = tree.find(self.YALE_CAS_URL + 'user').text.strip()
+        return tree
 
     def on_get_account(self, account):
         def to_set(s):
