17db75e843dfd20a64e289537446b992ae1aa44d
RTXUX <RTXUX@users.noreply.github.com>
2023-10-17 19:29:23 +0800

    neu: fix email validation

--- frontend/auth_providers/neu.py
+++ frontend/auth_providers/neu.py
@@ -15,7 +15,7 @@ class LoginView(ExternalLoginView):
 class GetCodeView(ExternalGetCodeView):
     provider = 'neu'
     duration = timedelta(hours=1)
-    validate_identity = UserRegexAndDomainEmailValidator('stu.neu.edu.cn', r'^((20(19|20|21|22)1?\d{4})|((19|20|21|22)\d{5}))$')
+    validate_identity = DomainEmailValidator('stu.neu.edu.cn')
 
 
 urlpatterns = [
